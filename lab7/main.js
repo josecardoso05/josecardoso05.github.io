cf8346778fc6bc816d02d03b9486fa02d68b5614
josecardoso <josebritocardoso05@gmail.com>
2025-11-02 11:34:02 +0000

    lab 7

--- lab7/main.js
+++ lab7/main.js
@@ -4,15 +4,58 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('produtos-selecionados', JSON.stringify([]));
     }
 
-    carregarProdutos(produtos)
-    atualizaCesto();
-})
+    fetch('https://deisishop.pythonanywhere.com/products/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro HTTP: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(produtos => {
+            console.log('Produtos recebidos da API:', produtos);
+            carregarProdutos(produtos);
+            atualizaCesto();
+
+            const selectCategoria = document.querySelector('#categorias');
+            const selectPreco = document.querySelector('#preco');
+            const inputPesquisa = document.querySelector('#pesquisa');
+            selectCategoria.addEventListener('change', () => {
+                carregarProdutos(produtos);
+            })
+            selectPreco.addEventListener('change', () => {
+                carregarProdutos(produtos);
+            })
+            inputPesquisa.addEventListener('input', () => {
+                carregarProdutos(produtos);
+            })
+        })
+        .catch(error => {
+            console.error('Erro ao carregar produtos:', error);
+        });
+});
 
 function carregarProdutos(produtos) {
     const artigo = document.querySelector('#produtos');
+    const selectCategorias = document.querySelector('#categorias');
+    const selectPreco = document.querySelector('#preco');
+    const inputPesquisa = document.querySelector('#pesquisa');
 
-    produtos.forEach(p => {
-        artigo.appendChild(criarProduto(p))
+    const categoria = selectCategorias.value;
+    const ordemPreco = selectPreco.value;
+
+    const textoPesquisa = inputPesquisa.value.toLowerCase();
+
+    let produtosFiltrados = produtos.filter(p => categoria === "todasCategorias" || p.category === categoria && p.title.toLowerCase().includes(textoPesquisa));
+
+    if (ordemPreco === "crescente") {
+        produtosFiltrados.sort((a, b) => a.price - b.price);
+    } else if (ordemPreco === "decrescente") {
+        produtosFiltrados.sort((a, b) => b.price - a.price);
+    }
+
+    artigo.innerHTML = '';
+    produtosFiltrados.forEach(p => {
+        artigo.appendChild(criarProduto(p));
     });
 }
 
@@ -60,13 +103,22 @@ function criarProduto(produto) {
 function atualizaCesto() {
     const produtosSelecionados = JSON.parse(localStorage.getItem('produtos-selecionados')) || [];
     const cesto = document.querySelector('#cesto');
+
+    const checkout = document.querySelector('#checkout');
+
     cesto.innerHTML = '';
+    checkout.innerHTML = '';
 
     produtosSelecionados.forEach(p => {
         const artigo = criaProdutoCesto(p);
         cesto.appendChild(artigo);
-        produtosSelecionados.setItem = cesto;
     });
+
+    const total = produtosSelecionados.reduce((soma, p) => soma + p.price, 0);
+
+    const precoTotal = document.createElement('h1');
+    precoTotal.textContent = 'Total: ' + total + '€';
+    checkout.appendChild(precoTotal)
 }
 
 function criaProdutoCesto(produto) {
@@ -100,9 +152,6 @@ function criaProdutoCesto(produto) {
     artigo.appendChild(imagem);
     artigo.appendChild(preco);
     artigo.appendChild(botao);
-
-
-    //fazer preço total
 
 
     return artigo;
